935ea5f02eeb1c71bc777d10da9e26137d67f724
Jaimit Patel <jaimit_12@hotmail.com>
2022-03-05 16:27:00 -0500

    added code to run the app

---
+++ dist/README.md
@@ -0,0 +1,33 @@
+# README Generator
+![GitHub license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+## Github URL 🌐
+[Jaimitpatel](https://github.com/Jaimitpatel/)
+## Description 📝
+This is a README generator that uses inquirer and node to allow users to create a readme for their projects.
+## Table of Contents 🗒
+* [Installations](#dependencies)
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributors](#contributors)
+* [Test](#test)
+## Installations (Dependencies) 💻
+To install dependencies, run these commands:
+```
+npm i
+```
+## Usage 🏆
+To install, input 'node index' in a command prompt in the root directory and go thru the questions. Your README will be saved in the dist/ folder.
+## License 📛
+      Copyright © MIT. All rights reserved. 
+      
+      Licensed under the MIT license.
+## Contributors 😃
+Jaimit Patel
+Contact me at jaimit_12@hotmail.com
+## Tests 🧪
+To run tests, run these commands:
+```
+npm test
+```
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What's the name of your project?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What's your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your email?"
+    },
+ 
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the installation instructions:",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors of this repo?",
+    }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// Function call to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((inquirerAnswers) => {
+            console.log("Generating.... Please wait....");
+            writeToFile(`./dist/README.md`, generateMarkdown({ ...inquirerAnswers }));
+        })
+}
+
 init();
